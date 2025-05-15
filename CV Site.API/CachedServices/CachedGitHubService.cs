3ead219990c2c4aa2bcfa4c9f8710795e223f426
rivka2211 <r0548500974@gmail.com>
2025-05-16 00:13:20 +0300

    add CachedGitHubService

---
+++ CV Site.API/CachedServices/CachedGitHubService.cs
@@ -0,0 +1,36 @@
+﻿using CV_Site.Service;
+using CV_Site.Service.Entities;
+using Octokit;
+
+namespace CV_Site.API.CachedServices
+{
+    public class CachedGitHubService:IGitHubService
+    {
+        private readonly IGitHubService _gitHubService;
+
+        public CachedGitHubService(IGitHubService gitHubService)
+        {
+            _gitHubService = gitHubService;
+        }
+
+        Task<List<PortfolioRepository>> IGitHubService.GetPortfolioAsync()
+        {
+           return _gitHubService.GetPortfolioAsync();
+        }
+
+        Task<int> IGitHubService.GetUserFollowesAsync(string userName)
+        {
+            return _gitHubService.GetUserFollowesAsync(userName);
+        }
+
+        Task<List<PortfolioRepository>> IGitHubService.SearchRepositoriesAsync(string? repositoryName, string? language, string? username)
+        {
+            return _gitHubService.SearchRepositoriesAsync(repositoryName,language,username);
+        }
+
+        Task<List<Repository>> IGitHubService.SearchRepositoriesInCSharp()
+        {
+            return _gitHubService.SearchRepositoriesInCSharp();
+        }
+    }
+}
